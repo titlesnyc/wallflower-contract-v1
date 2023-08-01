e0dd960d7c4e5dc20efc2337ca61cea4b9e12302
Parker Seagren <parkerseagren@gmail.com>
2023-08-01 11:25:01 -0400

    deployment mainnet differences

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,25 +4,34 @@
         const EDITION_CONTRACT_NAME = "TitlesEditionV1"
         const DEPLOYER_CONTRACT_NAME = "TitlesPublisherV1"
 
+        const [defaultDeployer, prodDeployer] = await ethers.getSigners();
+
         // Addresses
         const splitMainEthereum = '0x2ed6c4B5dA6378c7897AC67Ba9e43102Feb694EE';
+        const titlesControllerGoerli = '0xEA7c0d05AE85a02ADd86c0FC7b348c997C0B1fF4' // GOERLI (deployer)
+        const titlesControllerMainnet = '0x38b3C65800382Ff3Da70243C5AeFC0A4adA9f97f' // MAINNET (titlesadmin.eth)
 
-        const titlesController = '0xEA7c0d05AE85a02ADd86c0FC7b348c997C0B1fF4' // GOERLI (deployer)
-        // const titlesController = '0x38b3C65800382Ff3Da70243C5AeFC0A4adA9f97f' // MAINNET (titlesadmin.eth)
+        // ======================================================
+        //    GOERLI / MAINNET CONFIG
+        const deployerAddress = defaultDeployer
+        const titlesController = titlesControllerGoerli
+        // ======================================================
 
         // Config values
         const distributorFee = 5000
         const secondaryRoyalty = 1000
 
         // Deploy implementation
-        const implementation = await hre.ethers.deployContract(EDITION_CONTRACT_NAME);
+        const editionFactory = await ethers.getContractFactory(EDITION_CONTRACT_NAME)
+        const implementation = await editionFactory.connect(deployerAddress).deploy();
 
         // Deploy deployer
-        const deployer = await hre.ethers.deployContract(DEPLOYER_CONTRACT_NAME, [splitMainEthereum, titlesController, distributorFee, secondaryRoyalty, implementation.address]);
-    
+        const publisherFactory = await ethers.getContractFactory(DEPLOYER_CONTRACT_NAME)
+        const deployer = await publisherFactory.connect(deployerAddress).deploy(splitMainEthereum, titlesController, distributorFee, secondaryRoyalty, implementation.address);
+
         await implementation.deployed();
         await deployer.deployed();
-    
+
         console.log(`🚀 Deployed implementation contract at ::::::: ${implementation.address}`)
         console.log(`🚀 Deployed deployer contract at ::::::::::::: ${deployer.address}`);
     } catch (err) {
